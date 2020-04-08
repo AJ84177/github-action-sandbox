15510db491e7c76d05051c59bbddd6ab075d8810
panesar1810 <panesar.pb08@gmail.com>
2020-04-08 02:07:09 -0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learning';
+
+    disc: string;
+    todos: Todo[];
+
+    ngOnInit() {
+        this.todos = [];
+        this.disc = '';
+    }
+
+    addTodo(): void {
+        if (this.disc != '') {
+            const newTodo:Todo = {
+                todoId: Date.now(),
+                disc: this.disc,
+                isDone: false
+            };
+            this.todos.push(newTodo);
+        }
+        this.disc = '';
+        console.log('new todo added');
+    }
+
+    deleteTodo(todoId: number): void {
+        this.todos = this.todos.filter(todo => todo.todoId !== todoId);
+        console.log('todo ' + todoId + ' deleted');
+    }
+
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,5 @@
+export interface Todo {
+	todoId: number;
+	disc: string;
+	isDone: boolean;
+}
